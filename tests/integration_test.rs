837ba66f871038f379282006e04bcc9211679f83
Glyn Normington <glyn.normington@gmail.com>
2025-01-29 09:39:30 +0000

    Add integration test for multi-shot server
    Passes on macOS without code changes.
    
    Fails on Linux with:
    
    thread 'main' panicked at src/bin/spawn_client_test_helper.rs:25:71:
    connect failed: Os { code: 2, kind: NotFound, message: "No such file or directory" }
    stack backtrace:
       0: rust_begin_unwind
                 at /rustc/90b35a6239c3d8bdabc530a6a0816f7ff89a0aaf/library/std/src/panicking.rs:665:5
       1: core::panicking::panic_fmt
                 at /rustc/90b35a6239c3d8bdabc530a6a0816f7ff89a0aaf/library/core/src/panicking.rs:74:14
       2: core::result::unwrap_failed
                 at /rustc/90b35a6239c3d8bdabc530a6a0816f7ff89a0aaf/library/core/src/result.rs:1700:5
       3: core::result::Result<T,E>::expect
                 at /home/glyn/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/core/src/result.rs:1061:23
       4: spawn_client_test_helper::main
                 at ./src/bin/spawn_client_test_helper.rs:25:33
       5: core::ops::function::FnOnce::call_once
                 at /home/glyn/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/core/src/ops/function.rs:250:5
    note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
    test spawn_multi_shot_server_clients ... FAILED
    
    failures:
    
    ---- spawn_multi_shot_server_clients stdout ----
    thread 'spawn_multi_shot_server_clients' panicked at tests/integration_test.rs:67:5:
    child process failed with exit status code 101
    stack backtrace:
       0: rust_begin_unwind
                 at /rustc/90b35a6239c3d8bdabc530a6a0816f7ff89a0aaf/library/std/src/panicking.rs:665:5
       1: core::panicking::panic_fmt
                 at /rustc/90b35a6239c3d8bdabc530a6a0816f7ff89a0aaf/library/core/src/panicking.rs:74:14
       2: integration_test::await_child_process
                 at ./tests/integration_test.rs:67:5
       3: integration_test::spawn_multi_shot_server_clients
                 at ./tests/integration_test.rs:48:5
       4: integration_test::spawn_multi_shot_server_clients::{{closure}}
                 at ./tests/integration_test.rs:37:37
       5: core::ops::function::FnOnce::call_once
                 at /home/glyn/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/core/src/ops/function.rs:250:5
       6: core::ops::function::FnOnce::call_once
                 at /rustc/90b35a6239c3d8bdabc530a6a0816f7ff89a0aaf/library/core/src/ops/function.rs:250:5
    note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -20,21 +20,49 @@ use std::{env, process};
 #[cfg(not(any(feature = "force-inprocess", target_os = "android", target_os = "ios")))]
 #[test]
 fn spawn_one_shot_server_client() {
-    let executable_path: String = env!("CARGO_BIN_EXE_spawn_client_test_helper").to_string();
-
     let (server, token) =
         IpcOneShotServer::<String>::new().expect("Failed to create IPC one-shot server.");
 
-    let mut command = process::Command::new(executable_path);
-    let child_process = command.arg(token).arg("test message".to_string());
-
-    let mut child = child_process
-        .spawn()
-        .expect("Failed to start child process");
+    let child = spawn_client_test_helper(token, "test message".to_string());
 
     let (_rx, msg) = server.accept().expect("accept failed");
     assert_eq!("test message", msg);
 
+    await_child_process(child);
+}
+/// Test spawing two processes which then act as clients to a
+/// server in the parent process.
+#[cfg(not(any(feature = "force-inprocess", target_os = "android", target_os = "ios")))]
+#[test]
+fn spawn_multi_shot_server_clients() {
+    let (server, token) =
+        IpcOneShotServer::<String>::new().expect("Failed to create IPC one-shot server.");
+
+    let child1 = spawn_client_test_helper(token.clone(), "test message 1".to_string());
+    await_child_process(child1);
+
+    let (rx, msg1) = server.accept().expect("accept failed");
+    assert_eq!("test message 1", msg1);
+    
+    let child2 = spawn_client_test_helper(token, "test message 2".to_string());  
+    await_child_process(child2);
+
+    let msg2 = rx.recv().expect("failed to receive message 2");
+    assert_eq!("test message 2", msg2);
+}
+
+#[cfg(not(any(feature = "force-inprocess", target_os = "android", target_os = "ios")))]
+fn spawn_client_test_helper(server_name: String, message: String) -> process::Child {
+    let executable_path: String = env!("CARGO_BIN_EXE_spawn_client_test_helper").to_string();
+    process::Command::new(executable_path)
+        .arg(server_name)
+        .arg(message)
+        .spawn()
+        .expect("Failed to start child process")
+}
+
+#[cfg(not(any(feature = "force-inprocess", target_os = "android", target_os = "ios")))]
+fn await_child_process(mut child: process::Child) {
     let result = child.wait().expect("wait for child process failed");
     assert!(
         result.success(),
